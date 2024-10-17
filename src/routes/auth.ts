6e2ce7160cca352b2dec5cfb6a25663c17fd2e1b
Aniket Mandloi <aniketmandloi@Anikets-MacBook-Air.local>
2024-10-17 19:16:06 +0530

    add register logic

---
+++ src/routes/auth.ts
@@ -0,0 +1,23 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import { PassportStatic } from "passport";
+import { AuthService } from "../services/AuthService";
+
+const router = express.Router();
+
+const authRouter = (app: Express, passport: PassportStatic) => {
+  app.use("/api/auth", router);
+
+  // Registration Endpoint
+  router.post(
+    "/register",
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const data = req.body;
+        const response = await AuthService.register(data);
+        res.status(200).send(response);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+};
---
+++ src/services/AuthService.ts
@@ -0,0 +1,22 @@
+import createError from "http-errors";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const AuthService = {
+  async register(data: any) {
+    const { email } = data;
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+      if (user) {
+        throw createError(409, "User already exists");
+      }
+      return await prisma.user.create(data);
+    } catch (err: any) {
+      throw createError(500, err.message);
+    }
+  },
+};
