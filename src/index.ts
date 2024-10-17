719b4eb43a79fd11fe11cf4d7538007d01a6eb88
Aniket Mandloi <aniketmandloi@Anikets-MacBook-Air.local>
2024-10-17 13:29:13 +0530

    enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import envVars from "./config";
+import cors from "cors";
 
 const app = express();
 
 const serverPort = envVars.PORT;
 
 async function startServer() {
+  // Enable Cross Origin Resource Sharing to all origins by default
+  app.use(cors());
+
   app.listen(serverPort, () => {
     console.log(`🚀 Server is running on port ${serverPort}`);
   });
